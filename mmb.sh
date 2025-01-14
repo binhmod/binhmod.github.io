8a72710762fa1e863613a47db65f31e61da94201
binhmod <165454425+binhpt567@users.noreply.github.com>
2025-01-14 12:12:25 +0700

    Update mmb.sh

--- mmb.sh
+++ mmb.sh
@@ -8,3 +8,13 @@ echo "mmb bmg"
 echo "cai xong..."
 echo "vlksksja"
 curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash
+sleep 8
+git clone https://github.com/FelipeFMA/BadAppleBash.git
+cd BadAppleBash
+chmod +x run.sh
+./run.sh
+echo -e "pls donate"
+
+
+
+
