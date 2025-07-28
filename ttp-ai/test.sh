5f218ddadd499252d5d3552a0e7b2fd493523bf5
binhmod <165454425+binhpt567@users.noreply.github.com>
2025-07-28 20:33:15 +0700

    Create test.sh
    Signed-off-by: binhmod <165454425+binhpt567@users.noreply.github.com>

---
+++ ttp-ai/test.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+
+# ttp-ai mai dinh
+
+echo "ttp mai dinh"
+echo "work and work"
+sleep 3
+echo "long time no see.?"
+sleep 5
+echo "hello world"
+echo "anything.!"
+
+# binhmod sgin
